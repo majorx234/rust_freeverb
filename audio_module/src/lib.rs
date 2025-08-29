04329103933d68fb319570383aa6ec06b77e02f6
MajorX234 <majorx234@googlemail.com>
2025-08-29 17:27:07 +0200

    Add AudioProcessor and CommandHandler traits

--- audio_module/src/lib.rs
+++ audio_module/src/lib.rs
@@ -2,6 +2,13 @@ pub mod parameters;
 pub mod widget;
 
 use crate::parameters::ParameterProvider;
+
+pub trait CommandHandler {}
+
+pub trait AudioProcessor: CommandHandler {
+    fn process_stereo(&mut self, input: &[f32], output: &[f32]);
+}
+
 pub trait AudioModule: ParameterProvider {
-    
+    type Processor: AudioProcessor;
 }
