19c43b86e132fade2a82f45f665e35ffc67eb5b4
MajorX234 <majorx234@googlemail.com>
2025-09-04 23:36:44 +0200

    Add CommandHandler trait fix AudioProcessor process fct

--- audio_module/src/lib.rs
+++ audio_module/src/lib.rs
@@ -3,12 +3,20 @@ pub mod widget;
 
 use crate::parameters::ParameterProvider;
 
-pub trait CommandHandler {}
+pub enum Command {
+    SetParameter(usize, f32),
+}
+
+pub trait CommandHandler {
+    fn handle_command(&mut self, command: Command);
+}
 
 pub trait AudioProcessor: CommandHandler {
-    fn process_stereo(&mut self, input: &[f32], output: &[f32]);
+    fn process_stereo(&mut self, input: &[f32], output: &mut [f32]);
 }
 
 pub trait AudioModule: ParameterProvider {
     type Processor: AudioProcessor;
+
+    fn create_processor(sample_rate: usize) -> Self::Processor;
 }
