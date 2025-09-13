2c67c1137d4bad1112f5c466a7be5e8a2bbe8c16
MajorX234 <majorx234@googlemail.com>
2025-09-13 23:04:23 +0200

    Fix pass jack_thread handle to app

--- freeverb_egui/src/main.rs
+++ freeverb_egui/src/main.rs
@@ -19,9 +19,10 @@ fn main_run<Module: AudioModule>() {
     let options = eframe::NativeOptions::default();
 
     let samplerate = 48000;
-    let _jack_thread_hdl = start_jack_thread(samplerate, rx_command, rx1_close);
+    let jack_thread_hdl = start_jack_thread(samplerate, rx_command, rx1_close);
 
-    let freeverb_egui_app = FreeverbEguiApp::new::<Module>(Some(tx_close), Some(tx_command));
+    let freeverb_egui_app =
+        FreeverbEguiApp::new::<Module>(Some(jack_thread_hdl), Some(tx_close), Some(tx_command));
     let _ = eframe::run_native(
         "FreeverbEgui App",
         options,
